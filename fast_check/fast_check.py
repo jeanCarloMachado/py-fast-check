5e80c6143a626616ffed374b3f88f1f70bbc7986
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-05-19 08:48:48 +0200

    cleanups

--- fast_check/fast_check.py
+++ fast_check/fast_check.py
@@ -1,27 +1,40 @@
 import os
 import subprocess
+
+
 class FastCheck:
     project_path = None
 
     def run(self, path=None, mypy_enabled=False):
-        if not path:
-            path = '.'
+        if path:
+            # change directory
+            os.chdir(path)
 
         print("Compiling the project")
         os.system(f"python -m compileall -q {path}")
-        print("Running Tests")
-        os.system("pytest")
 
         if mypy_enabled:
             print("Running type checker")
             os.system(f"mypy {path} ")
-        print("Formatting the project")
-        os.system(f"ruff check {path} ")
+        print("Running ruff")
+        os.system("ruff clean")
+        os.system(f"ruff format {path} ")
+        os.system(f"ruff check {path} --fix")
+
+        if not os.path.exists("tests"):
+            print("No tests folder found")
+
+        print("Running Tests")
+        os.system("pytest")
 
         print("Done!")
 
     def select_and_run(self):
-        project = subprocess.check_output("cd /Users/jean.machado/projects ; ls | fzf ", shell=True, text=True)
+        project = subprocess.check_output(
+            "cd /Users/jean.machado/projects ; ls | /Users/jean.machado/.fzf/bin/fzf ",
+            shell=True,
+            text=True,
+        )
         self.set_current_project(f"/Users/jean.machado/projects/{project.strip()}")
 
         self.run()
@@ -31,17 +44,23 @@ class FastCheck:
 
     def set_current_project(self, project_path: str):
         from tiny_data_warehouse import DataWarehouse
-        import pandas as pd
 
         # write event
         tdw = DataWarehouse()
-        tdw.write_event('fast_check_current_project', {'path': project_path})
+        tdw.write_event("fast_check_current_project", {"path": project_path})
 
     def get_current_project(self):
         from tiny_data_warehouse import DataWarehouse
+
         # write event
         tdw = DataWarehouse()
-        return tdw.event('fast_check_current_project').iloc[-1].to_dict()['path']
+        project_path = (
+            tdw.event("fast_check_current_project").iloc[-1].to_dict()["path"]
+        )
+
+        print("Current project: ", project_path)
+
+        return project_path
 
 
 def main():
